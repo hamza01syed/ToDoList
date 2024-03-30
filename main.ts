746e491a0ab2d16cadcfd536e1749779f8b47f9d
hamza01syed <127969573+hamza01syed@users.noreply.github.com>
2024-03-30 10:35:48 -0700

    first commit

---
+++ main.ts
@@ -0,0 +1,22 @@
+import inquirer from "inquirer";
+let todos=[];
+let condition=true;
+
+while(condition){
+let addTask=await inquirer.prompt([{
+    name:"todo",
+    message:"what do you want to add in your list?",
+    type:"input"
+},{
+    name:"addMore",
+    message:"Are you sure you want to add more in todos",
+    type:"confirm",
+    default:"false"
+}
+])
+// console.log(addTask);
+todos.push(addTask.todo);
+
+condition=addTask.addMore
+console.log(todos)
+}
